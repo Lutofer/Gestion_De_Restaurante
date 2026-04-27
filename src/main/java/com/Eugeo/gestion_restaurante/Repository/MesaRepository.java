150155b5a7bbae3eb03f4a7de62003294fa61bcd
Maria Collana <U22231576@utp.edu.pe>
2026-04-26 23:52:59 -0500

    feat: Implementa CRUD de mesa (entity, repository, service, controller)(Closes #1)

---
+++ src/main/java/com/Eugeo/gestion_restaurante/Repository/MesaRepository.java
@@ -0,0 +1,23 @@
+package com.Eugeo.gestion_restaurante.Repository;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.Eugeo.gestion_restaurante.Entity.EstadoMesa;
+import com.Eugeo.gestion_restaurante.Entity.Mesa;
+
+
+@Repository
+public interface MesaRepository extends JpaRepository<Mesa, Long>{
+
+    Optional<Mesa> findByNumero(Integer numero);
+
+    List<Mesa> findByEstado(EstadoMesa estado);
+
+    boolean existsByNumero(Integer numero);
+}
+
