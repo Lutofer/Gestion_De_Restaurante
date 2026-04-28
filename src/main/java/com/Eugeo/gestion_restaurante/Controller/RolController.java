8a2ba544ee2a548ad6908fa058855d05e1d089b4
Loana Muñoz <u22314058@utp.edu.pe>
2026-04-27 23:16:52 -0500

    feat: implementa CRUD de Rol(entity, repository, service, controller) (Closes #4)

---
+++ src/main/java/com/Eugeo/gestion_restaurante/Controller/RolController.java
@@ -0,0 +1,25 @@
+package com.Eugeo.gestion_restaurante.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Eugeo.gestion_restaurante.Entity.Rol;
+import com.Eugeo.gestion_restaurante.Service.RolService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/roles")
+@RequiredArgsConstructor
+public class RolController {
+    private final RolService service;
+
+    @GetMapping
+    public List<Rol> findAll(){
+        return service.findAll();
+    }
+
+}
